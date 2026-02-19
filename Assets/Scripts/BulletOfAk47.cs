13c5b5d79eb51e9c629b650d660f6c8fa31443e6
Carlo Maria Proietti <carlo.maria.proietti@outlook.it>
2026-02-19 12:42:43 +0100

    no comment

--- Assets/Scripts/BulletOfAk47.cs
+++ Assets/Scripts/BulletOfAk47.cs
@@ -1,22 +1,22 @@
 using UnityEngine;
 
-public class BulletOfPistol : Bullet
+public class BulletOfAk47 : Bullet
 {
     public int damage = 25;
-    public float speed = 2.0f;
+    public float speed = 10.0f;
     public Rigidbody2D rigidBody;
     public override int getDamageAmount()
     {
-        throw new System.NotImplementedException();
+        return damage;
     }
 
     public override Rigidbody2D getRigidBody()
     {
-        throw new System.NotImplementedException();
+        return rigidBody;
     }
 
     public override float getSpeed()
     {
-        throw new System.NotImplementedException();
+        return speed;
     }
 }
