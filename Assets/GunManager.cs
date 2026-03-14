80e409c1671b67bffd5011bff8e69a54a9b4b7d8
Carlo Maria Proietti <carlo.maria.proietti@outlook.it>
2026-03-14 15:51:12 +0100

    both player and enemy shoot correctly

--- Assets/GunManager.cs
+++ Assets/GunManager.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 
+//this function instantiates the gun of an ENEMY, the choose of the gun is done runtime.
+//it is simple to custom this class to randomly choose a gun between many.
 public class SingleGunManager : MonoBehaviour
 
 {
