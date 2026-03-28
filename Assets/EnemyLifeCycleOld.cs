f515c81a75c1eae1ac467e77b14af7a4caa34e88
Giovanni-Santomarco <gio.santomarco@stud.uniroma3.it>
2026-03-28 18:29:41 +0100

    fixed the few problems with the death enemy spawn

---
+++ Assets/EnemyLifeCycleOld.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class EnemyLifeCycleOld : MonoBehaviour
+{
+    public int maxHealth = 100;
+    private int currentHealth;
+    internal BoardManager levelManager;
+    public GameObject Rapace_morto;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        // Add death animation or sound here TODO
+
+        // destroy the object
+        Transform spotOfDeath = gameObject.transform;
+
+        Destroy(gameObject);
+
+        //tell the current level to spawn a dead enemy at the spot in which its health went down to 0
+        levelManager.SpawnDeathEnemy(Rapace_morto, spotOfDeath);
+
+        //tell current level about the death, a death triggers a check by LevelManager, i.e. isNewLevel?
+        levelManager.decreaseEnemies();
+    }
+}
