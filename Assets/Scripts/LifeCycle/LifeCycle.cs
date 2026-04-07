3c6bffe94fb7369671cb3b71c6632e58a68d6dd5
Giovanni-Santomarco <gio.santomarco@stud.uniroma3.it>
2026-04-07 14:46:49 +0200

    added animation when either the player or the enemy get hit (they turn reddish for a fraction of second)

--- Assets/Scripts/LifeCycle/LifeCycle.cs
+++ Assets/Scripts/LifeCycle/LifeCycle.cs
@@ -5,10 +5,13 @@ public abstract class LifeCycle : MonoBehaviour
     public int maxHealth = 100;
     protected int currentHealth;
     internal LevelManager levelManager;
+    internal DamageFeedback damageFeedback;
 
     void Start()
     {
         currentHealth = maxHealth;
+
+        damageFeedback = GetComponent<DamageFeedback>();
     }
 
     public void TakeDamage(int damage)
@@ -18,6 +21,8 @@ public abstract class LifeCycle : MonoBehaviour
         {
             Die();
         }
+
+        damageFeedback.StartHitAnimation();
     }
 
     public abstract void Die();
