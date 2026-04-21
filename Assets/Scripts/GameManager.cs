8bafa19df1e2d40cb368d4017faa891fa074f1cf
Carlo Maria Proietti <carlo.maria.proietti@outlook.it>
2026-04-21 21:11:43 +0200

    exit button correctly works

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,7 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public LevelManager boardScript;
+    public GameObject gameOverMenuUI;
+    public LevelManager levelManager;
     public Animator transitionAnimator;
     private float transitionTime = 2f;
     private int level = 1;
@@ -19,39 +20,41 @@ public class GameManager : MonoBehaviour
 
     void InitGame()
     {
-        boardScript.GenerateLevel(level);
+        levelManager.GenerateLevel(level);
     }
     IEnumerator InitGame1()
     {
-        // 1. Play the first animation (Cover the screen)
+        // Cover the screen
         transitionAnimator.SetTrigger("StartTransition");
 
-        // 2. Wait for the animation to completely finish
+        // Wait for the animation to completely finish
         // We use WaitForSecondsRealtime so it works even if you paused the game (Time.timeScale = 0)
         yield return new WaitForSecondsRealtime(transitionTime);
 
         // 3. Perform the heavy lifting while the screen is covered
-        boardScript.GenerateLevel(level);
+        levelManager.GenerateLevel(level);
 
-        // Optional: Add a tiny extra wait time here if you want the screen 
-        // to stay covered for a split second after generating, just for pacing.
+        // Add a tiny extra wait time here 
+        // to stay covered for a split second after generating.
         yield return new WaitForSecondsRealtime(0.2f);
 
-        // 4. Play the second animation (Reveal the screen)
+        // Reveal the screen
         transitionAnimator.SetTrigger("EndTransition");
     }
 
     IEnumerator GameOver()
     {
-        // 1. Play the first animation (Cover the screen)
-        transitionAnimator.SetTrigger("StartTransition");
+        
+        // Cover the screen
+        //transitionAnimator.SetTrigger("StartTransition");
 
         // 2. Wait for the animation to completely finish
         // We use WaitForSecondsRealtime so it works even if you paused the game (Time.timeScale = 0)
         yield return new WaitForSecondsRealtime(transitionTime);
 
         // 3. Perform the heavy lifting while the screen is covered
-        SceneManager.LoadScene("MainMenu");
+        //SceneManager.LoadScene("MainMenu");
+        gameOverMenuUI.SetActive(true);
     }
     void Update()
     {
@@ -79,5 +82,10 @@ public class GameManager : MonoBehaviour
     internal void playerIsDead()
     {
         isPlayerDead = true;
+    }
+
+    public void onExitButton()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
