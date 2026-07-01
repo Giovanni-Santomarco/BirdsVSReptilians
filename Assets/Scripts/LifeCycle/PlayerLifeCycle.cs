5d9a337998249d66b01bf3cdf0ee0b424a2bcfad
Giovanni-Santomarco <gio.santomarco@stud.uniroma3.it>
2026-07-01 12:35:34 +0200

    solved a bug in which enemies during chase phase appeared to not move but still the animation was on. added player dead model after being defeated

--- Assets/Scripts/LifeCycle/PlayerLifeCycle.cs
+++ Assets/Scripts/LifeCycle/PlayerLifeCycle.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class PlayerLifeCycle : LifeCycle
 {
+    public GameObject playerDead;
+
     public override void Die()
     {
+        Transform spotOfDeath = gameObject.transform;
+
+        Destroy(gameObject);
+
+        levelManager.SpawnDrop(playerDead, spotOfDeath);
+
         levelManager.playerDies();
     }
 
